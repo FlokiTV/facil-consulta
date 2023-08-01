703f5f4212e9b97ea697a3d1d91bf0236d814f07
FlokiTV <dawysonw@gmail.com>
2023-08-01 10:17:18 +0000

    add: `/pacientes/{id_paciente}` route

--- tests/index.js
+++ tests/index.js
@@ -5,10 +5,35 @@ const $axios = axios.create({
   baseURL,
 });
 
-$axios.post("pacientes", {
-    "nome": "Matheus Henrique",
-    "cpf": "795.429.940-60",
-    "celular": "(11) 98432-5789"
-}).then(data =>{
-  console.log(data.data)
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const $paciente = {
+  "nome": "Matheus Henrique",
+  "cpf": `${randomInt(111, 999)}.${randomInt(111, 999)}.${randomInt(111, 999)}-${randomInt(11, 99)}`,
+  "celular": "(11) 98432-5789"
+}
+test("pacientes", async ($) => {
+  let pacienteID;
+  await $.test("Create Paciente", async () => {
+    try {
+      let { data } = await $axios.post("pacientes", $paciente);
+      pacienteID = data.id
+      console.log(data);
+    } catch (error) {
+      throw new Error(error.response.data.message);
+    }
+  })
+
+  await $.test("Update Paciente", async () => {
+    if (!pacienteID) throw new Error("pacienteID cant be null");
+
+    let { data } = await $axios.put("pacientes/" + pacienteID, {
+      "nome": "Luana Rodrigues Garcia",
+      "celular": "(11) 98484-6362"
+    });
+    
+    console.log(data)
+  })
 })
