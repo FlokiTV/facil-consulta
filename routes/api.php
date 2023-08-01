19141fc43427168862d473f69f9beebb73cf470b
FlokiTV <dawysonw@gmail.com>
2023-08-01 17:07:35 +0000

    update: middleware `auth:api` on protected routes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,22 @@ use App\Models\Cidade;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if (!$token = auth()->attempt($credentials)) {
+        abort(401, "Unauthorized");
+    }
+    return response()->json([
+        'data' => [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]
+    ]);
 });
 // Cidades
 Route::get('/cidades', function () {
@@ -31,22 +45,22 @@ Route::get('/cidades', function () {
 */
 // Listar médicos
 Route::get('/medicos', [MedicoController::class, 'all']);
-// Adicionar medico
-Route::post('/medicos', [MedicoController::class, 'store']);
 // Listar médicos de uma cidade
 Route::get('/cidades/{id_cidade}/medicos', [MedicoController::class, 'getByCidade']);
+// Adicionar medico
+Route::middleware('auth:api')->post('/medicos', [MedicoController::class, 'store']);
 /*
     Pacientes
 */
 // Adicionar paciente
-Route::post('/pacientes', [PacienteController::class, 'store']);
+Route::middleware('auth:api')->post('/pacientes', [PacienteController::class, 'store']);
 // Atualizar paciente
-Route::post('/pacientes/{id_paciente}', [PacienteController::class, 'update']); // No teste diz POST
-Route::put('/pacientes/{id_paciente}', [PacienteController::class, 'update']); // No Postman o teste é com PUT
+Route::middleware('auth:api')->post('/pacientes/{id_paciente}', [PacienteController::class, 'update']); // No teste diz POST
+Route::middleware('auth:api')->put('/pacientes/{id_paciente}', [PacienteController::class, 'update']); // No Postman o teste é com PUT
 /*
     Relação MedicoPacientes
 */
 // Listar pacientes do médico
-Route::get('/medicos/{id_medico}/pacientes', [MedicoPacienteController::class, 'getPacientesVinculados']);
+Route::middleware('auth:api')->get('/medicos/{id_medico}/pacientes', [MedicoPacienteController::class, 'getPacientesVinculados']);
 // Vincular paciente e médico
-Route::post('/medicos/{id_medico}/pacientes', [MedicoPacienteController::class, 'vincularPaciente']);
+Route::middleware('auth:api')->post('/medicos/{id_medico}/pacientes', [MedicoPacienteController::class, 'vincularPaciente']);
