f269aa69cd35c56327a68e64e8b92371466a4535
FlokiTV <dawysonw@gmail.com>
2023-08-03 21:37:46 +0000

    add: `cidades` tests

---
+++ tests/cidades.js
@@ -0,0 +1,15 @@
+import test from "node:test";
+import { Schema, $axios } from "./config.js";
+
+test("Teste de rota: cidades", async ($) => {
+    let $data;
+    await $.test("Listar cidades", async () => {
+        try {
+            let { data } = await $axios.get("/cidades");
+            $data = data
+        } catch (error) {
+            console.log(error.response.status);
+        }
+        Schema.cidade.parse($data);
+    });
+});
