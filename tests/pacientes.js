ebb3604fe021dae75c02191eab5344d06964391a
FlokiTV <dawysonw@gmail.com>
2023-08-03 21:38:46 +0000

    update: `pacientes` tests with zod

--- tests/pacientes.js
+++ tests/pacientes.js
@@ -1,34 +1,42 @@
 import test from "node:test";
-import assert from "node:assert";
-import { login, randomInt } from "./config.js";
+import { Schema, login, randomInt } from "./config.js";
+
+const $paciente = {
+  "nome": "Matheus Henrique",
+  "cpf": `${randomInt(111, 999)}.${randomInt(111, 999)}.${randomInt(111, 999)}-${randomInt(11, 99)}`,
+  "celular": "(11) 98432-5789"
+}
 
 test("Teste de rota: pacientes", async ($) => {
   let $axios;
-  const $paciente = {
-    "nome": "Matheus Henrique",
-    "cpf": `${randomInt(111, 999)}.${randomInt(111, 999)}.${randomInt(111, 999)}-${randomInt(11, 99)}`,
-    "celular": "(11) 98432-5789"
-  }
+  let $data;
   let id = 0;
+  
   await $.test("Autenticação", async () => {
     $axios = await login();
   });
+
   await $.test("Adicionar paciente", async () => {
     try {
       let { data } = await $axios.post("/pacientes", $paciente);
       id = data.id
+      $data = data
     } catch (error) {
-      assert.equal(error.response.status, 401, "'/medicos/1/pacientes' should return 401");
+      console.log(error.response.status);
     }
-  })
+    Schema.paciente.parse($data);
+  });
+
   await $.test("Atualizar paciente", async () => {
     try {
-      await $axios.put("/pacientes/"+id, {
+      let { data } = await $axios.put("/pacientes/" + id, {
         nome: "Luana Rodrigues Garcia",
         celular: "(11) 98484-6362"
       });
+      $data = data;
     } catch (error) {
-      assert.equal(error.response.status, 401, "'/medicos/1/pacientes' should return 401");
+      console.log(error.response.status);
     }
-  })
+    Schema.paciente.parse($data);
+  });
 });
